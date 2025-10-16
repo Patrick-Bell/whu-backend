1561f65a48dbb2ea6044835df2ebee7ad3b43e38
Patrick Bell <patrick@Patricks-Laptop.local>
2025-10-16 12:55:31 +0100

    iupdate

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,14 @@
 require 'sidekiq'
 require 'sidekiq-cron'
 
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
 Sidekiq::Cron::Job.create(
   name: 'Daily Upcoming Fixtures Email - every day at midnight',
   cron: '0 0 * * *',
